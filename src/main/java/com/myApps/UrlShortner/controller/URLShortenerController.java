202fdb3c2eac8f24258cf22f9b2d96e4bc57883c
Hrithik H V <hrithiksv98@gmail.com>
2025-06-18 13:21:56 +0530

    adding ratelimiter

--- src/main/java/com/myApps/UrlShortner/controller/URLShortenerController.java
+++ src/main/java/com/myApps/UrlShortner/controller/URLShortenerController.java
@@ -3,11 +3,13 @@ package com.myApps.UrlShortner.controller;
 import com.myApps.UrlShortner.responsdto.UrlResponseDto;
 import com.myApps.UrlShortner.service.URLResolver;
 import com.myApps.UrlShortner.service.URLShortener;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/shortener")
+
 public class URLShortenerController {
 
     private final URLShortener urlShortener;
@@ -19,6 +21,7 @@ public class URLShortenerController {
     }
 
     @PostMapping
+    @RateLimiter(name = "urlShortener")
     public ResponseEntity<UrlResponseDto> createShortUrl(@RequestParam("url") String Url){
         UrlResponseDto urlResponseDto = urlShortener.shortenUrl(Url);
 
@@ -26,12 +29,14 @@ public class URLShortenerController {
     }
 
     @GetMapping("/resolve/{code}")
+    @RateLimiter(name = "urlShortener")
     public ResponseEntity<UrlResponseDto> getShortUrl(@PathVariable("code") String code){
         UrlResponseDto resolve = urlResolver.resolve(code);
         return ResponseEntity.ok().body(resolve);
     }
 
     @PutMapping("/{code}")
+    @RateLimiter(name = "urlShortener")
     public ResponseEntity<UrlResponseDto> updateUrl(@PathVariable("code") String shortCode, @RequestParam("url") String url){
         UrlResponseDto urlResponseDto = urlShortener.updateUrl(shortCode, url);
 
@@ -39,6 +44,7 @@ public class URLShortenerController {
     }
 
     @DeleteMapping("/{code}")
+    @RateLimiter(name = "urlShortener")
     public ResponseEntity<UrlResponseDto> deleteUrl(@PathVariable("code") String code){
         urlShortener.Delete(code);
         return ResponseEntity.ok().build();
